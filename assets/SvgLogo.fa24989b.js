846e266a83b654c461306e4c1f3216abf68825db
Jeff Schofield <jeff@jeffschofield.com>
2022-05-16 21:19:25 -0400

    chore: Compress images

--- assets/SvgLogo.fa24989b.js
+++
@@ -1,1 +0,0 @@
-import{a as o,c as e,l as s,v as t,f as l}from"./vendor.fd246b83.js";var a="/assets/logo.1021709d.svg";const c={class:"aspect-[1.63/1]"},n={viewBox:"0 0 512 312",class:"w-full h-full fill-current"},r=["xlink:href"],p=o({setup(_){return(i,f)=>(e(),s("div",c,[(e(),s("svg",n,[t("use",{"xlink:href":`${l(a)}#logo`},null,8,r)]))]))}});export{p as _};
--- assets/fullscreen.dec14691.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./index.de39d3cb.js";import{s}from"./vendor.fd246b83.js";const t={};function o(r,f){return s(r.$slots,"default")}var c=e(t,[["render",o]]);export{c as default};
